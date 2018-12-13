7821d89342123deaef3bde00556c6fcf274e9cfa
Bradyn Poulsen <bradyn@bradynpoulsen.com>
2018-12-13 16:28:26 -0700

    Blocking SDK coroutine builders that utilize coroutine dispatchers

---
+++ java-sdk/src/main/kotlin/com/github/bradynpoulsen/aws/coroutines/AwsBlocking.kt
@@ -0,0 +1,29 @@
+package com.github.bradynpoulsen.aws.coroutines
+
+import com.amazonaws.AmazonWebServiceRequest
+import com.amazonaws.AmazonWebServiceResult
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.EmptyCoroutineContext
+
+/**
+ * Executes the blocking [method] in the [context] (defaults to [Dispatchers.IO]).
+ */
+suspend inline fun <AwsRequest : AmazonWebServiceRequest, AwsResult : AmazonWebServiceResult<*>> awsBlockingCoroutine(
+    crossinline method: (request: AwsRequest) -> AwsResult,
+    context: CoroutineContext = EmptyCoroutineContext,
+    crossinline requestBuilder: () -> AwsRequest
+): AwsResult = withContext(Dispatchers.IO + context) {
+    method(requestBuilder())
+}
+
+/**
+ * Executes the blocking [method] in the [context] (defaults to [Dispatchers.IO]).
+ */
+suspend inline fun <AwsResult : AmazonWebServiceResult<*>> awsBlockingCoroutine(
+    crossinline method: () -> AwsResult,
+    context: CoroutineContext = EmptyCoroutineContext
+): AwsResult = withContext(Dispatchers.IO + context) {
+    method()
+}
